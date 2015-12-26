0bdc95abc4953f5b1139211048fc1cdbe75d4332
Tsubasa Namiki <namikingsoft@gmail.com>
2015-12-26 14:22:46 +0900

    Resolve chapter01.knock01

--- src/NLP100/Chapter01.hs
+++ src/NLP100/Chapter01.hs
@@ -10,3 +10,13 @@ module NLP100.Chapter01 where
 knock00 :: String -> String
 knock00 [] = ""
 knock00 (x:xs) = knock00 xs ++ [x]
+
+-- | 01. 「パタトクカシーー」
+--
+-- 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
+--
+-- >>> knock01 "stressed"
+-- "desserts"
+--
+knock01 :: String -> String
+knock01 n = [n!!0] ++ [n!!2] ++ [n!!4] ++ [n!!6]
--- test/NLP100/Chapter01Spec.hs
+++ test/NLP100/Chapter01Spec.hs
@@ -9,6 +9,14 @@ spec = do
   describe "第1章: 準備運動" $ do
 
     describe "00. 文字列の逆順" $ do
+      -- 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
 
       it "should return correct value" $ do
         knock00 "stressed" `shouldBe` "desserts"
+
+    describe "01. 「パタトクカシーー」" $ do
+      -- 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して
+      -- 連結した文字列を得よ．
+
+      it "should return correct value" $ do
+        knock01 "パタトクカシーー" `shouldBe` "パトカー"
